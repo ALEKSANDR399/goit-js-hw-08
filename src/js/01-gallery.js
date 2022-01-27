86e194cc602f6b77bfbf4f3c4fb50dfccd717bc3
ALEKSANDR399 <88842048+ALEKSANDR399@users.noreply.github.com>
2022-01-27 12:32:39 +0200

    ДЗ№8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBoxForCardImage = document.querySelector('.gallery');
+const cardCollection = addCardWithImg(galleryItems);
+
+galleryBoxForCardImage.addEventListener('click', addOpenOriginalSizeImage);
+
+galleryBoxForCardImage.innerHTML = cardCollection;
+
+function addCardWithImg(cards) {
+  return cards
+    .map(
+      ({ original, preview, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`,
+    )
+    .join('');
+}
+
+function addOpenOriginalSizeImage(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) return;
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+}
