93b061fe189c8bb987f7b3c79d3eea1dab6e22f6
ALEKSANDR399 <88842048+ALEKSANDR399@users.noreply.github.com>
2022-01-29 17:13:37 +0200

    дз№8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
 
+const playerBox = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(playerBox);
+const dataTimeAfterPause = 'time-after-pause';
+
+player.setCurrentTime(localStorage.getItem('time-after-pause'));
+
+player.on('pause', savePauseTimeInMemory);
+
+function savePauseTimeInMemory(data) {
+  localStorage.setItem('time-after-pause', data.seconds);
+}
