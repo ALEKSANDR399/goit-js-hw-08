44ab67e6fb87d5d8c3d0a4c61d41cccbf1bb369e
ALEKSANDR399 <88842048+ALEKSANDR399@users.noreply.github.com>
2022-01-30 10:05:26 +0200

    ДЗ№8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
+
 const formBox = document.querySelector('.feedback-form');
+const inputEmail = document.querySelector('input');
+const textareaMessage = document.querySelector('textarea');
+const buttonSubmit = document.querySelector('button');
+const textInMemory = localStorage.getItem('feedback-email-state');
+const formObjectForStorage = {};
+
+formBox.addEventListener('submit', sendFormSubmit);
+formBox.addEventListener('input', throttle(addEmailToLocalStorage, 500));
+
+function addEmailToLocalStorage(event) {
+  formObjectForStorage[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-message-state', JSON.stringify(formObjectForStorage));
+}
+
+function sendFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  formData.forEach((value, name) => {
+    if (value === '') {
+      alert('no empty fields allowed');
+    }
+    console.log('name:', name);
+    console.log('value:', value);
+  });
+  event.target.reset();
+  localStorage.removeItem('feedback-message-state');
+}
+
+function safeNoSendForm() {
+  const savedFormData = localStorage.getItem('feedback-message-state');
+  const parsedFormData = JSON.parse(savedFormData);
+  if (parsedFormData) {
+    inputEmail.value = parsedFormData.email || '';
+    textareaMessage.value = parsedFormData.message || '';
+  }
+}
+safeNoSendForm();
+// cloud2265@gmail.com
